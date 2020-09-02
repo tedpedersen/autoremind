d217dc36565dfced8b5e8b560324764483d99fe0
Ted Pedersen <tedpedersen@me.com>
2020-09-01 19:55:08 -0700

    adding model edit inputs

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,32 @@ $(".edit-button").click(function() {
     console.log(selectDate);
 });
 
+$(".edit-appt-button").click(function(){
+    $(".edit-appt-button").addClass("hide");
+    $(".delete-appt-button").addClass("hide");
+    $(".save-edit-appt-button").removeClass("hide");
+    $(".cancel-edit-appt-button").addClass("hide");
+    $(".name-input").removeAttr("readonly");
+    $(".date-input").removeAttr("readonly");
+});
+
+$(".save-edit-appt-button").click(function(){
+    /* osborn - send the data here */
+    $(".edit-appt-button").removeClass("hide");
+    $(".delete-appt-button").removeClass("hide");
+    $(".save-edit-appt-button").addClass("hide");
+    $(".cancel-edit-appt-button").addClass("hide");
+    $(".name-input").attr("readonly");
+    $(".date-input").attr("readonly");
+});
+
+$(".cancel-edit-appt-button").click(function(){
+    $(".edit-appt-button").removeClass("hide");
+    $(".delete-appt-button").removeClass("hide");
+    $(".save-edit-appt-button").addClass("hide");
+    $(".cancel-edit-appt-button").addClass("hide");
+    $(".name-input").attr("readonly");
+    $(".date-input").attr("readonly");
+});
+
 
